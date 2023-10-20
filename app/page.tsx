b7421d3d4c26d992e6a72571e2f1d1c51abb3cbb
Johneriksson88 <john.e.eriksson@gmail.com>
2023-10-20 03:05:19 +0000

    Add todo functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
+import { TodoItem } from "@/components/TodoItem";
 import { prisma } from "@/db";
 import Link from "next/link";
-import { todo } from "node:test";
+
+function getTodos() {
+  return prisma.todo.findMany();
+}
+
+async function toggleTodo(id: string, complete: boolean) {
+  "use server";
+
+  await prisma.todo.update({ where: { id }, data: { complete } });
+}
 
 export default async function Home() {
-  const todos = await prisma.todo.findMany();
-  // await prisma.todo.create({ data: { title: "test", complete: false } });
+  const todos = await getTodos();
   return (
     <>
       <header className="flex justify-between mb-4 items-center">
@@ -21,7 +30,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
         ))}
       </ul>
     </>
